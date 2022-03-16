2e75ba7b8b9c022fdcbc84e704c89aee1345f743
jor22 <jorge22ac@gmail.com>
2022-03-16 19:43:12 -0300

    Add product card component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import Detail  from './Components/Detail'
+
 
 function App() {
   return (
     <div className="App">
-     <Detail />
     </div>
   );
 }
