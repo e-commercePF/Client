b91821fdf50a6b1c70fa52e07cfe5da79fc15163
Eric Illanes <e.icuriqueo@gmail.com>
2022-04-03 00:27:51 -0400

    primera versión reviews

--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -1,7 +1,7 @@
 import {
     GET_PRODUCTS, SEARCH_PRODUCTS, GET_DETAILS, ADD_CART, CLEAR_CART, DELETE_ONE_ITEM_FROM_CART, ADD_TO_FAVORITES, DELETE_FROM_FAVORITES, Get_ALL_FAVORITES,
     DELETE_ALL_SINGLE_ITEM_FROM_CART, SET_USER, CLEAN_DETAIL, EDIT_THE_PRODUCT, GET_BRAND, GET_CATEGORIES,
-    GET_ALL_USERS, DELETE_ONE_ITEM_FROM_STOCK, UPDATE_USERS, GET_PRODUCT_PAGINADO, FILTER_PRICE, FILTER_BY, IS_ADMIN
+    GET_ALL_USERS, DELETE_ONE_ITEM_FROM_STOCK, UPDATE_USERS, GET_PRODUCT_PAGINADO, FILTER_PRICE, FILTER_BY, IS_ADMIN, GET_SHOP
 } from "./actions"
 
 const initialState = {
@@ -19,7 +19,8 @@ const initialState = {
     isAdmin: false,
     orders: [],
     pages: 10,
-    filters: {}
+    filters: {},
+    myShop: []
 
 }
 export default function rootReducer(state = initialState, action) {
@@ -213,6 +214,11 @@ export default function rootReducer(state = initialState, action) {
                 orders: action.payload
             }
 
+        case GET_SHOP:
+            return {
+                ...state,
+                myShop: action.payload
+            }
         default:
             return state
     }
