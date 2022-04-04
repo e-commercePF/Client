d98ca11c75837632fba39b497d5282742f47b360
Eric Illanes <e.icuriqueo@gmail.com>
2022-04-04 00:53:34 -0400

    avance previsualizar imagen

--- src/Components/CreatePage.jsx
+++ src/Components/CreatePage.jsx
@@ -121,36 +121,6 @@ export default function Formulario() {
                 resetForm("")
             }
         },
-
-        formHandler: (e) => {
-            e.preventDefault();
-            const file = e.target[0].formik.files[0];
-            uploadFiles(file);
-        },
-        uploadFiles: (file) => {
-            //
-            if (!file) return;
-            const sotrageRef = ref(storage, `files/${file.name}`);
-            const uploadTask = uploadBytesResumable(sotrageRef, file);
-
-            uploadTask.on(
-                "state_changed",
-                (snapshot) => {
-                    const prog = Math.round(
-                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-                    );
-                    setProgress(prog);
-                },
-                (error) => console.log(error),
-                () => {
-                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                        setImg([...imagen, downloadURL])
-                        console.log("File available at", downloadURL);
-                    });
-                }
-            );
-        },
-
         initialValues: {
             name: "",
             description: "",
